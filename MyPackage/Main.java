358e12e8c09ed515316b7c93a7eba79c9d887040
Bohdan Kachmar <bohdanka4mar@gmail.com>
2022-07-05 15:56:37 +0300

    My Homework (GTP-003)

--- MyPackage/Main.java
+++ MyPackage/Main.java
@@ -2,6 +2,6 @@ package MyPackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi  World!");
+        System.out.println("My Homework: add changes to Hello World file in separate branch, create merge request, merge changes to main branch into your GitHub repo and send me the link, delete branch");
     }
 }
