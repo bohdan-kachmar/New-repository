67b32fa73f841b2e79272db0920b9c4efd11052e
Bohdan Kachmar <bohdanka4mar@gmail.com>
2022-07-05 14:45:21 +0300

    GTP-002: New commit

--- MyPackage/Main.java
+++ MyPackage/Main.java
@@ -2,6 +2,6 @@ package MyPackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello New World!");
+        System.out.println("Hello  World 23!");
     }
 }
