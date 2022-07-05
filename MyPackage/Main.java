ff90369a494916c68074869c1acd364231595256
Bohdan Kachmar <bohdanka4mar@gmail.com>
2022-07-05 14:48:32 +0300

    Changes according to commit

--- MyPackage/Main.java
+++ MyPackage/Main.java
@@ -2,6 +2,6 @@ package MyPackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello  World 23!");
+        System.out.println("Hi  World!");
     }
 }
